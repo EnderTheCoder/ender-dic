51a580b539c2f1161a840866f7092e5e351d9c2d
EnderTheCoder <1991455223@qq.com>
2021-03-24 00:58:12 +0800

    version-beta
    第一版代码

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$filename = '3500.txt';
+$self = 'test.php';
+$interval = 50;
+$width = 35;
+$cellpadding = 3;
+$font_size = 28;
+
+$db_name = 'python';
+$sql_user = 'ender2';
+$sql_passwd = 'Feng,HK,4778!';
+
+$word_length = 100;
+?>
